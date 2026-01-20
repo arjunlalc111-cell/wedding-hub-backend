73a547e7e8b0704998c0df7a2ba73f29af72f539
arjun patel <arjunlalc111@gmail.com>
2026-01-20 06:12:01 -0800

    Removed root models/ after move to src/models/

--- models/Banner.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-const bannerSchema = new mongoose.Schema({
-  image: String,
-  title: String,
-  link: String,
-  createdAt: { type:Date, default:Date.now }
-});
-export default mongoose.model("Banner", bannerSchema);
--- models/Coupon.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-const couponSchema = new mongoose.Schema({
-  code: { type:String, required:true, unique:true },
-  discount: Number,
-  validTill: Date
-});
-export default mongoose.model("Coupon", couponSchema);
